a8a4a0dbabf0f6f31cc785c572371f26bd0a67bc
krpk1900 <krpk1900@gmail.com>
2020-12-09 23:52:24 +0900

    [Improve] システムスペックを改善 #2

--- spec/support/login_macros.rb
+++ spec/support/login_macros.rb
@@ -1,8 +1,9 @@
 module LoginMacros
-  def login_as(user)
-    visit admin_login_identifier
+  def login(user)
+    visit admin_login_identifier_path
     fill_in 'user[name]',	with: user.name
     click_button '次へ'
     fill_in 'user[password]',	with: 'password'
     click_button 'ログイン'
   end
+end
--- spec/system/admin_articles_previews_spec.rb
+++ spec/system/admin_articles_previews_spec.rb
@@ -1,27 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe "AdminArticlesPreviews", type: :system do
-  let(:admin){ create(:user, :admin) }
-  before{ login_as(admin) }
-
-  let(:article){ create(:article) }
+  let(:admin) { create(:user, :admin) }
 
   describe '記事作成画面で画像ブロックを追加' do
-    describe 'プレビュー' do
-      context "画像を添付しないでプレビュー " do
-        it '正常にプレビューページが表示される' , focus:true do
-          click_link '記事'
-          click_link '新規作成'
-          fill_in 'タイトル', with: '記事タイトル'
-          fill_in 'スラッグ', with: 'test'
-          fill_in '概要',	with: '記事概要'
-          click_button '登録する'
-          click_link 'ブロックを追加する'
-          click_on '画像'
-          click_link 'プレビュー'
-          switch_to_window(windows.last)
-          expect(page).to have_content '記事タイトル'
-        end
+    context '画像を添付せずにプレビューを閲覧' do
+      it '正常に表示される' do
+        login(admin)
+        click_link '記事'
+        click_link '新規作成'
+        fill_in 'タイトル', with: 'テスト記事'
+        fill_in 'スラッグ', with: 'test'
+        fill_in '概要',	with: '記事概要'
+        click_button '登録する'
+        click_link 'ブロックを追加する'
+        click_link '画像'
+        click_link 'プレビュー'
+        switch_to_window(windows.last)
+        expect(page).to have_content 'テスト記事'
       end
     end
   end
