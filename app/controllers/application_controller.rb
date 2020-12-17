34fb8b1cce18b45d1626856aa98c2ea619b2ff00
krpk1900 <krpk1900@gmail.com>
2020-12-18 03:06:02 +0900

    [Modify] ステータスコードの表記を403から:forbiddenに修正 #14

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -59,6 +59,6 @@ class ApplicationController < ActionController::Base
   end
 
   def render_403
-    render file: Rails.root.join('public/403.html'), status: 403, layout: false, content_type: 'text/html'
+    render file: Rails.root.join('public/403.html'), status: :forbidden, layout: false, content_type: 'text/html'
   end
 end
