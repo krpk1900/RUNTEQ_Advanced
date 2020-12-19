b94ebc2c12d1e48098bfd824a18a752a7a6737cb
krpk1900 <krpk1900@gmail.com>
2020-12-20 03:48:21 +0900

    [Add] デフォルト値を700に設定 #16

--- db/migrate/20201219174322_add_eye_catch_width_to_artilcles.rb
+++ db/migrate/20201219174322_add_eye_catch_width_to_artilcles.rb
@@ -1,6 +1,6 @@
 class AddEyeCatchWidthToArtilcles < ActiveRecord::Migration[5.2]
   def up
-    add_column :articles, :eye_catch_width, :integer
+    add_column :articles, :eye_catch_width, :integer, default: 700
   end
 
   def down
