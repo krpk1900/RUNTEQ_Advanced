2d55563cefb973096958d6fb7ef167d3a4d2febc
krpk1900 <krpk1900@gmail.com>
2020-12-21 08:05:09 +0900

    [Add] twitterの場合の条件分岐を追加 #18

--- app/decorators/article_block_decorator.rb
+++ app/decorators/article_block_decorator.rb
@@ -5,7 +5,11 @@ module ArticleBlockDecorator
     elsif medium?
       '<i class="fa fa-image"></i>'.html_safe
     elsif embed?
-      '<i class="fa fa-youtube-play"></i>'.html_safe
+      if blockable.youtube?
+        '<i class="fa fa-youtube-play"></i>'.html_safe
+      elsif blockable.twitter?
+        '<i class="fa fa-twitter"></i>'.html_safe
+      end
     end
   end
 
