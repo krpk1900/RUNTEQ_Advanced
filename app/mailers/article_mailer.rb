05fa2120dba547188d50af5e286b0340ad5d4057
krpk1900 <krpk1900@gmail.com>
2020-12-25 10:22:49 +0900

    [Mosify] 変数名を変更 #23

--- app/mailers/article_mailer.rb
+++ app/mailers/article_mailer.rb
@@ -1,9 +1,7 @@
 class ArticleMailer < ApplicationMailer
   def report_summary
-    @published_articles_num = Article.published.count
-
-    @published_articles_yesterday = Article.yesterday
-    @published_articles_num_yesterday = @published_articles_yesterday.count
+    @published_articles_count = Article.published.count
+    @articles_published_yesterday = Article.published_yesterday
     mail(to: 'admin@example.com', subject: '公開済記事の集計結果')
   end
 end
